93f6bbad1e173678942e4fecc6384df10df04eeb
Micah <micahshallom@gmail.com>
2024-07-07 22:34:20 +0100

    fix error

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 func main() {
 	//load dotenv
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	//initialize config
